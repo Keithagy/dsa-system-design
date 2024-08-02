c6881f01c0b58668a4224a4b55b3f67b3b021754
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-08-02 12:23:36 +0800

    git-ignore old practice

--- .old/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/keithagy/leetcode-practice
-
-go 1.21.3
--- .old/src/dsa_base/q001_linear_search.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-def linear_search(haystack: List[int], needle: int) -> bool:
-    for i in range(len(haystack)):
-        num = haystack[i]
-        if num == needle:
-            return True
-    return False
