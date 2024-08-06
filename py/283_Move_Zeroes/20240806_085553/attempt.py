accb840925ce98399d573f2dd06c1b691cf855a7
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-08-06 09:02:44 +0800

    revise grind169 w2-1

---
+++ py/283_Move_Zeroes/20240806_085553/attempt.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero_pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if i != zero_pos:
+                    nums[zero_pos], nums[i] = nums[i], nums[zero_pos]
+                zero_pos += 1
+
