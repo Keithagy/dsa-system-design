d3fc65bd920e8a67033a4659cce09f3f29d864eb
Keith Ang <keithang@Keiths-MacBook-Pro.local>
2024-09-19 11:58:30 +0800

    revise grind169 w1-22

---
+++ go/876_Middle_of_the_Linked_List/20240919_115409/attempt.go
@@ -0,0 +1,26 @@
+package q876
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
